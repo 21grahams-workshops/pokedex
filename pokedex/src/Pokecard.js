ba706dad005b9513e38c86ffe91a3e042acf3c3b
21grahams <graham.kirsh@gmail.com>
2021-05-12 16:37:13 -0600

    building functionality to devise a winner between game

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-// const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
-const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+// const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`; // smaller distorted images
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`; // better quality images
 
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 class Pokecard extends Component {
   render() {
 
-    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    // let imgSrc = `${POKE_API}${this.props.id}.png`; // worse images
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`; // better images
 
     return (
       <div className='Pokecard'>
